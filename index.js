3443d8dc756325e36e818a70df91506d428ad730
Ryan Frazin <RyanFrazin@Gmail.com>
2025-07-29 20:00:21 -0700

    Add css to header

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const port = 3000;
 var blogPosts = [];
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { blogPosts });
