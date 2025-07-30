1e63544fef2260f1ae37cbea249762e685b8d6ee
Ryan Frazin <RyanFrazin@Gmail.com>
2025-07-29 20:42:56 -0700

    Add functionality that stops blank titles from being added to blogPosts

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { title } from "process";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,18 +12,18 @@ const port = 3000;
 var blogPosts = [];
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { blogPosts });
-})
+  res.render("index.ejs", { blogPosts });
+});
 
 app.post("/submit", (req, res) => {
-    const { title, content } = req.body;
+  const { title, content } = req.body;
 
-    const safeTitle = title.replace(/[^a-zA-Z0-9-_]/g, '');
-    const fullTemplate = `
+  const safeTitle = title.replace(/[^a-zA-Z0-9-_]/g, "");
+  const fullTemplate = `
     <html>
         <body>
             <h1>${title}</h1>
@@ -33,45 +33,49 @@ app.post("/submit", (req, res) => {
         </body>
     </html>`;
 
-    fs.writeFile(`./views/posts/${safeTitle}.ejs`, fullTemplate, err => {
-        if(err) {
-            console.error('Error writing file:', err);
-            return res.status(500).send('Could not create blog post file.');
-        }
-        
-        blogPosts.push(safeTitle);
-        res.render("index.ejs", { blogPosts });
-    })
-})
+  if (safeTitle == "") {
+    res.render("index.ejs", { blogPosts });
+  } else {
+    fs.writeFile(`./views/posts/${safeTitle}.ejs`, fullTemplate, (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return res.status(500).send("Could not create blog post file.");
+      }
+
+      blogPosts.push(safeTitle);
+      res.render("index.ejs", { blogPosts });
+    });
+  }
+});
 
 app.get("/post", (req, res) => {
-    const { title } = "Post Title";
+  const { title } = "Post Title";
 
-    res.render('post.ejs', { title });
-})
+  res.render("post.ejs", { title });
+});
 
 app.get("/posts/:title", (req, res) => {
-    const { title } = req.params;
-    const filePath = path.join(__dirname, 'views', 'posts', `${title}.ejs`);
+  const { title } = req.params;
+  const filePath = path.join(__dirname, "views", "posts", `${title}.ejs`);
 
-    if(!fs.existsSync(filePath)) {
-        return res.status(404).send("Post not found");
-    }
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("Post not found");
+  }
 
-    res.render(`posts/${title}`);
-})
+  res.render(`posts/${title}`);
+});
 
 app.get("/edit/:title", (req, res) => {
-    const title = req.params.title || "Post Title";
-    const index = blogPosts.findIndex(post => post == title);
+  const title = req.params.title || "Post Title";
+  const index = blogPosts.findIndex((post) => post == title);
 
-    if(index != -1) {
-        blogPosts.splice(index, 1);
-    }
+  if (index != -1) {
+    blogPosts.splice(index, 1);
+  }
 
-    res.render('post.ejs', { title });
-})
+  res.render("post.ejs", { title });
+});
 
 app.listen(port, (req, res) => {
-    console.log(`Now listening on port ${port}`);
-})
+  console.log(`Now listening on port ${port}`);
+});
